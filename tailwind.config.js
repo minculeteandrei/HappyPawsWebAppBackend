7e3975b279e33f43006ad8c4d00bbfe82724a087
minculeteandrei <minculeteandrei@gmail.com>
2022-04-20 16:56:06 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgba(13, 186, 13, 0.69)'
+      },
+      fontFamily: {
+        body: ['Inria+Sans']
+      }
+    },
+  },
+  plugins: [],
+}
