65f06d90d84121255688e393d6369f32b2d82222
minculeteandrei <minculeteandrei@gmail.com>
2022-05-16 22:25:41 +0300

    actual project structure

---
+++ src/test/java/com/happypaws/HappyPawsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happypaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPawsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
