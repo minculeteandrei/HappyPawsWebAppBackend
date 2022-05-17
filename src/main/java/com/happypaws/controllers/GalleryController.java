6fd0f284851442e243100a82c2a6520337aa8483
minculeteandrei <minculeteandrei@gmail.com>
2022-05-18 02:59:46 +0300

    added backend, modified contact, servicii si gallery, added footer and navigation

---
+++ src/main/java/com/happypaws/controllers/GalleryController.java
@@ -0,0 +1,13 @@
+package com.happypaws.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+
+    @GetMapping
+    public String getGallery(){return "gallery";}
+}
---
+++ src/main/java/com/happypaws/controllers/ServicesController.java
@@ -0,0 +1,13 @@
+package com.happypaws.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/services")
+public class ServicesController {
+
+    @GetMapping
+    public String getServices(){return "services";}
+}
