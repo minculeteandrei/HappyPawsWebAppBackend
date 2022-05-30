6a79c774f9bcf56145a89e25815504142f48cb56
minculeteandrei <minculeteandrei@gmail.com>
2022-05-30 23:02:10 +0300

    idk why

--- src/main/java/com/happypaws/controllers/HomeController.java
+++ src/main/java/com/happypaws/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.happypaws.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,4 +11,13 @@ public class HomeController {
 
     @GetMapping
     public String home(){return "home";}
+
+    @GetMapping("login")
+    public String getLoginPage(){return "login";}
+
+    @GetMapping("login-error")
+    public String getLoginError(Model model){
+        model.addAttribute("loginError", true);
+        return "login";
+    }
 }
---
+++ src/main/java/com/happypaws/domain/User.java
@@ -0,0 +1,6 @@
+package com.happypaws.domain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+}
